b080956f75e46e3d0e7cac76ad9b1ac3e516eaba
zeel-codder <zeelprajapati321@gmail.com>
2021-08-29 00:04:58 +0530

    done

---
+++ bot.js
@@ -0,0 +1,55 @@
+
+const twit = require('twit')
+
+require('dotenv').config();
+
+const config = {
+    consumer_key: process.env.API_Key,
+    consumer_secret: process.env.API_Secret_Key,
+    access_token: process.env.Access_Token,
+    access_token_secret: process.env.Access_Token_Secret
+
+}
+
+const T = new twit(config)
+
+function retweet(searchText) {
+
+    let params = {
+        q: searchText + '',
+        result_type: 'mixed',
+        count: 50,
+    }
+
+    T.get('search/tweets', params, function (err_search, data_search, response_search) {
+
+        let tweets = data_search.statuses
+        if (!err_search) {
+            let tweetIDList = []
+            for (let tweet of tweets) {
+                tweetIDList.push(tweet.id_str);
+            }
+
+            // Call the 'statuses/retweet/:id' API endpoint for retweeting EACH of the tweetID
+            for (let tweetID of tweetIDList) {
+                T.post('statuses/retweet/:id', { id: tweetID }, function (err_rt, data_rt, response_rt) {
+                    if (!err_rt) {
+                        // console.log("\n\nRetweeted!)
+                    }
+                    else {
+                        // console.log("\nError... Duplication maybe... " + tweetID)
+                        console.log("Error = " + err_rt)
+                    }
+                })
+            }
+        }
+        else {
+            console.log("Error while searching" + err_search)
+            process.exit(1)
+        }
+    })
+}
+
+// Run every 60 seconds
+setInterval(function () { retweet('#60daysofdsa OR #100DaysOfCode'); }, 60000)
+// setInterval(function () { retweet('#zeel11'); }, 60000)
